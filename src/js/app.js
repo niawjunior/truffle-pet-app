3d3cd47d8735b6a88884ddf8c06e43d49161c5c6
niawjunior <pasupol@dome.cloud>
2018-03-15 08:20:36 +0700

    Instantiating the contract

--- src/js/app.js
+++ src/js/app.js
@@ -37,10 +37,17 @@ web3 = new Web3(App.web3Provider);
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
-
+    $.getJSON('Adoption.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+    
+      // Set the provider for our contract
+      App.contracts.Adoption.setProvider(App.web3Provider);
+    
+      // Use our contract to retrieve and mark the adopted pets
+      return App.markAdopted();
+    });
     return App.bindEvents();
   },
 
